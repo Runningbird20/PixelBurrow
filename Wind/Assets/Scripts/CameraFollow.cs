7283f7099ccb9d96427a5460578a4725bb1b1795
Runningbird20 <153043342+Runningbird20@users.noreply.github.com>
2026-03-07 00:21:47 -0500

    Camera Movement

---
+++ Wind/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 5, -6);
+    public float smooth = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 desired = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desired, smooth * Time.deltaTime);
+
+        transform.LookAt(target);
+    }
+}
